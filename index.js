59c9fff36d7000afef8669510bc84e0faff5094b
jangdonghyuk <60882863+jangdonghyuk@users.noreply.github.com>
2021-01-16 19:34:07 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-Parser");
+const { User } = require("./models/User");
+
+const config = require("./config/key");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://dhj100:Dhj030534@cluster0.0yrpx.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected~"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World! ~ 안녕하세요. 아자카 술식 전개!");
+  res.send("Hello World! ~ 안녕하세요. 렌고쿠 쿄주로!");
+});
+
+app.post("/register", (req, res) => {
+  //회원가입 시 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어 준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
